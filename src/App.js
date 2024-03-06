e25dde3c88f3f138bf349842fe5e21f4d86493ee
pascalniri <pascalniri@gmail.com>
2024-03-06 08:01:09 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const QuestionComponent = () => {
+  const [questionVisible, setQuestionVisible] = useState(false);
+  const [answer, setAnswer] = useState();
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const handleOpenQuestion = () => {
+    setQuestionVisible(true);
+
+    setTimeout(() => {
+      setQuestionVisible(false);
+    }, 20000);
+  };
+
+  const handleSubmit = () => {
+
+    if (parseInt(answer) !== 2000000) {
+      setAlertMessage('Wrong Answer');
+    } else {
+      setAlertMessage('Correct Answer');
+    }
+    setAnswer('');
+   
+  };
+
+  const handleInputChange = (event) => {
+    setAnswer(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='subcontainer'>
+        <h1>ATTENTION:</h1>
+        <p>The Question lasts for two minutes, and then it closes</p>
+      </div>
+      <button id='open-btn' onClick={handleOpenQuestion}>Open Question</button>
+      {questionVisible && (
+        <div>
+          <h2>What answer do we get when we multiply 1000 by 2000?</h2>
+          <div className='question-field'>
+            <input
+              type="number"
+              placeholder='Answer'
+              value={answer}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleSubmit}>SUBMIT</button>
+          </div>
+          {alertMessage && (
+            <div className='alert'>
+              {alertMessage}
+            </div>
+            
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default QuestionComponent;
